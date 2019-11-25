ea5b2d5830645ed3c09f141ce3334713ccf48dc2
Nicholas Ferrara <nferrara100@gmail.com>
2019-11-25 16:00:01 +0000

    feat: moved settngs to json file

--- mergePull.js
+++ mergePull.js
@@ -2,18 +2,14 @@
 
 const axios = require("axios");
 const semver = require("semver");
+const settings = require("../../dependamerge.json");
 
 // Arguments provided by CI
 const pullRequestId = process.argv[2];
-const pullRequestSlug = process.argv[3];
-const githubToken = process.argv[4];
-
-// Flag certain updates for manual review
-const alwaysReviewRegex = /bump (react |react-dom |react-native |@types\/react |react-redux |@types\/react-test-renderer |react-test-renderer)/g;
-const reviewRegex = /bump (expo|react-native-gesture-handler |react-native-svg |react-navigation |react-native-reanimated |lottie-react-native |typescript |react-native-view-shot )/g;
+const githubToken = process.argv[3];
 
 // Axios options
-const pullRequestUrl = `https://api.github.com/repos/${pullRequestSlug}/pulls/${pullRequestId}`;
+const pullRequestUrl = `https://api.github.com/repos/${settings.githubUsername}/${settings.repoName}/pulls/${pullRequestId}`;
 const options = {
     headers: {
         "User-Agent": "node/https",
@@ -21,6 +17,9 @@ const options = {
     },
 };
 const mergeInput = {merge_method: "rebase"};
+
+const getCommitRegex = modules =>
+    new RegExp("bumps (" + modules.join(" |") + " )", "g");
 
 // Check contents of pull request via the Github API
 axios
@@ -36,13 +35,13 @@ axios
         }
 
         // Ignore pull request if title is one known to cause problems
-        if (title.match(alwaysReviewRegex)) {
+        if (title.match(getCommitRegex(settings.neverUpdate))) {
             console.log("Dependency should be reviewed manually");
             process.exit(0);
         }
 
         // Ignore pull request if upgrade is not a patch and on review list
-        if (title.match(reviewRegex)) {
+        if (title.match(getCommitRegex(settings.onlyUpdatePatches))) {
             const versions = title.match(/[0-9]+\.[0-9]+\.[0-9]+/g);
             const sameMajorVersion =
                 semver.major(versions[0]) === semver.major(versions[1]);
